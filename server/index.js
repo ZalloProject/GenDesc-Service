7ca6ec80e91226c0fc3f76731998ca6a6339b56b
Hack Reactor Student <student@hackreactor.com>
2019-03-27 15:22:14 -0700

    exporting server for testing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const House = require('../db/index');
+const {House} = require('../db/index');
 const app = express();
 
 app.use(express.static('./public'));
@@ -8,7 +8,7 @@ app.use(express.static('./public'));
 app.get('/houses/*', (req, res) => {
     let houseID = req.path.split('/');
     houseID = houseID[houseID.length - 1];
-    House.find({_id: houseID}, (err, docs) => {
+    House.findOne({_id: houseID}, (err, docs) => {
         res.send(docs);
     });
 });
@@ -19,3 +19,5 @@ const PORT = process.env.PORT || 9001
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
+
+module.exports = app;
