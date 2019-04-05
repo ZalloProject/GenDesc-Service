3e810a01394e8b07535530b17af9892d9b778009
James Kelly <jamesckelly99@gmail.com>
2019-04-04 17:24:10 -0700

    changed port

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/houses/*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
