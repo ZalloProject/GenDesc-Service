08e5709d2a890e92c7e51bbc6e8ce0c06eff7e97
James Kelly <jamesckelly99@gmail.com>
2019-04-04 17:28:50 -0700

    new port

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/houses/*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3050;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
