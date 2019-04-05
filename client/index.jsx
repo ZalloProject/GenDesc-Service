f52f9fb0c394513707c3a9349463dc9218231d71
James Kelly <jamesckelly99@gmail.com>
2019-04-04 17:20:29 -0700

    changed fetch

--- client/index.jsx
+++ client/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import GenDesc from './app.jsx';
 
 fetch(
-  `http://ec2-52-14-98-3.us-east-2.compute.amazonaws.com/houses/${Math.floor(Math.random() * 98)}`,
-  { mode: 'no-cors', 'Content-Type': 'application/json' }
+  `http://ec2-52-14-98-3.us-east-2.compute.amazonaws.com/houses/${Math.floor(Math.random() * 98)}`
 )
   .then(res => res.json())
   .then(house => ReactDOM.render(<GenDesc house={house} />, document.getElementById('gendesc')));
